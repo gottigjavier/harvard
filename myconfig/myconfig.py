f15349a6d2f37a8f15d776102f5e01987bd0b66a
gottigjavier <gottigjavier@gmail.com>
2020-06-24 17:41:50 -0300

    add files to this branch

---
+++ myconfig/myconfig.py
@@ -0,0 +1,4 @@
+SECRET_KEY = "this-should-be-a-random-key-for-heroku"
+DATABASE_URL = "postgres://nyzenldagzkzuj:a667a7c7564cffc091c55f53571462b03e528646db132643f4f44866b8898bf7@ec2-34-200-72-77.compute-1.amazonaws.com:5432/df8nbsbg5vfnql"
+
+#DATABASE_URL = "postgresql://postgres:local1pass3@127.0.0.1:5432/gottig"
