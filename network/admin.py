158c01e5a1b4c59d5ad58dfb96c5030daee5b27d
gottigjavier <gottigjavier@gmail.com>
2020-10-27 14:30:07 -0300

    add db.sqlite3 to git

--- network/admin.py
+++ network/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from .models import User
+
 # Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'date_joined', 'is_active', 'is_staff', 'is_superuser')
+    ordering = ('username',)
+    search_fields = ('username', 'email')
+    list_filter = ('is_staff', 'is_active', 'is_superuser', 'date_joined',)
+
+admin.site.register(User, UserAdmin)
+admin.site.site_header = "Network Site Administration"
